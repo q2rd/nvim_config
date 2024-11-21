7e97de7c11662940fd8e8bf1b389f751cc518b45
mbpyytcqw <egorrozhnov78@mail.ru>
2024-11-21 09:36:02 +0300

    fix tree view (git stage icons)

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -38,7 +38,7 @@ local nvim_tree = require "nvim-tree"
 
 nvim_tree.setup {
   git = {
-    enable = false, -- Использовать Git для файлового менеджера
+    enable = true, -- Использовать Git для файлового менеджера
     ignore = false, -- Отключить игнорирование файлов по правилам .gitignore
   },
   filters = {
